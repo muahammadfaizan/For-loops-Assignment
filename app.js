6aadcefe69435a93f1138a3535160a5910610215
Muhammad Faizan <fiazansuri855@gmail.com>
2024-03-26 22:20:40 +0500

    JS For-loops-Assignment

---
+++ app.js
@@ -0,0 +1,405 @@
+// console.log('for loops');
+
+
+
+
+             //  | For Loops |
+
+        //  Question & Answer
+
+
+
+//   Q.No-01 : Write a program to display the message “Hello World” 5 times
+//             in your browser using for loop.     
+
+//   Ans.No-01 :  
+
+// const hero = document.querySelector('#hero')
+
+// for(let i = 0 ; i < 5 ; i++){
+
+//     console.log(i)
+
+//     hero.innerHTML += `<p>${"hello world"}</p>`
+    
+// }
+
+
+
+
+
+
+
+
+
+//   Q.No-02 : Write a program to print numeric counting from 1 to 10.
+
+//    Ans.No-02 :
+
+// for(let i = 1 ; i <= 10 ; i++){
+//     console.log(i)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+//   Q.No-03 : Write a program to print multiplication table of any number
+//               using for loop. Table number & length should be taken as an
+//               input from user
+
+
+//    Ans.No-03 :
+
+// const input = prompt('enter your table number');
+// const times = prompt('how many times you want');
+
+
+// for(let i = 1 ; i <= times ; i++){
+//     console.log(`${input} * ${i} = ${input * i}`)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   Q.No-04 : You have an array
+//             A = [“Nokia”, “Samsung”, “Apple”, “Sony”, “Huawei”]
+//             Write each element on new line with the help of for loop
+  
+      
+ 
+//    Ans.No-04 : 
+
+// const arr = ["Nokia",'Samsung', "Apple", "Sony", "Huawei"];
+// const div = document.querySelector('#hero');
+
+
+// for(let i = 0 ; i < arr.length ; i++){
+//     console.log(arr[i]);
+
+//     div.innerHTML += `<p>${arr[i]}</p>`
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+//   Q.No-05 : Write a program to print items of the following array        using for loop:fruits = [“apple”, “banana”, “mango”, “orange”, “strawberry”]
+
+
+ 
+//   Ans.No-05 :
+
+
+// const fruits = ['apple' , 'banana' , 'mango' , 'orange' , 'strawberry'];
+// const div = document.querySelector('#hero');
+
+
+// for(let i = 0 ; i < fruits.length ; i++){
+//     console.log(fruits[i])
+
+//     div.innerHTML += `<p>${fruits[i]}</p>`
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
